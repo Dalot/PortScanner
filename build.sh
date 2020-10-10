770fc7dddd374f82a4078c5dac68d769a70ea802
Rogério Dalot <ge.dalot@gmail.com>
2020-10-10 11:17:05 +0100

    linked the sfml files to the compiled file

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-docker exec -it port_scanner bash -c "cd /opt/app && g++ -o Main Main.cpp && ./Main"
+docker exec -it port_scanner bash -c "cd /opt/app && g++ Main.cpp -o Main -lsfml-system -lsfml-network && ./Main"
